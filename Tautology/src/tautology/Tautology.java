bd763bef7e7eec5d9b5d87a1c301344e0c8b9a39
Absion <Absion@DESKTOP-LQ25KO9>
2016-08-30 21:22:47 -0500

    First Setup

---
+++ Tautology/src/tautology/Tautology.java
@@ -0,0 +1,34 @@
+
+package tautology;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Absion
+ */
+public class Tautology extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
